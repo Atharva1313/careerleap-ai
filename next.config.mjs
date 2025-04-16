b066ec33401b35aaec04754e6bb792b6adee63fc
atharva <atharvagupta1434@gmail.com>
2025-04-16 22:15:37 +0530

    Adding desgin to landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "randomuser.me",
+          pathname: "/**", // Allow all paths from this domain
+        },
+      ],
+      domains: ["images.unsplash.com"], // Allow simpler domain-based configuration
+    },
+  };
+  
+  export default nextConfig;
