ac622ac87b1684afe208f58f7d2f65980f5d141c
atharva <atharvagupta1434@gmail.com>
2026-06-25 16:54:29 +0530

    Fixed deployment errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,11 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
 const eslintConfig = [
-  "next/core-web-vitals",
+  ...compat.extends("next/core-web-vitals"),
 ];
 
 export default eslintConfig;
